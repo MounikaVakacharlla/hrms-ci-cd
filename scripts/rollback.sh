e4c7ed37b2008bf8694424fccdd3d2976bc9d4ea
MounikaVakacharlla <vakacharllamounika1014@gmail.com>
2026-06-30 11:49:06 +0530

    Newly Modified 1

--- scripts/rollback.sh
+++ scripts/rollback.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+
+VERSION=$1
+
+
+echo "Rollback started"
+
+
+docker stop hrms-app || true
+
+
+docker rm hrms-app || true
+
+
+VERSION=$VERSION docker compose up -d
+
+
+echo "Rollback completed"
